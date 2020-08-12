fde3a077ed4dfc35fdbd5ba191a983360abc5ad7
vetkje <larsgallardo@hotmail.no>
2020-08-12 10:44:14 +0200

    stuff1

--- test.py
+++ test.py
@@ -4,4 +4,11 @@ import matplotlib.pyplot as plt
 print("my ass")
 print("my ass")
 print("my ass")
-print("my ass")
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
